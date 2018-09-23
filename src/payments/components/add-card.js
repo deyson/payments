7cb8cf88067d5f15ee38a9a1aef530ec47a9e29e
Deyson García <deyson@gmail.com>
2018-09-23 13:39:43 -0500

    Add card dev

---
+++ src/payments/components/add-card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './add-card.css';
+
+function AddCardLayout(props) {
+    return (
+        <div>
+           <div className="paymentez-form" id="my-card" data-capture-name="true"></div>
+        </div>
+    )
+}
+
+export default AddCardLayout;
+
+
--- src/payments/containers/add-card.js
+++ src/payments/containers/add-card.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import AddCardLayout from '../components/add-card';
+import QueryString from 'query-string';
 
-class AddCard extends Component{
-    render(){
-       return (
-        <div>Add card</div>
+class AddCard extends Component {
+    state = {
+        uid: '',
+        email: ''
+    }
+    componentDidMount() {
+        console.log(location.search);
+        const parsed = QueryString.parse(location.search);
+        this.setState({
+            uid: parsed.uid,
+            email: parsed.email,
+        })
+    }
+    render() {
+        return (
+            <AddCardLayout />
         )
     }
 }
