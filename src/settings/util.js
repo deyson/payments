db68bfd29274c42218fd0fb3ba4e260956543304
Yojan Morales <yojan.morales@contractors.accruent.com>
2018-09-23 12:12:55 -0500

    util file was added

---
+++ src/settings/util.js
@@ -0,0 +1,22 @@
+
+function paymentezClientApplicationCode() {
+    return "EXITO-CO-CLIENT";
+}
+function paymentezClientAppKey() {
+    return "L0KxqMO32mRSBZkzkY71hG4dUWwPLM";
+}
+
+function paymentezServerApplicationCode() {
+    return "EXITO-CO-SERVER";
+}
+function paymentezServerAppKey() {
+    return "cvNBJXzsdcH4qpgLq7tlkdtaclIvp2";
+}
+module.exports =
+    {
+        paymentezClientApplicationCode,
+        paymentezClientAppKey,
+        paymentezServerApplicationCode,
+        paymentezServerAppKey
+
+    }
